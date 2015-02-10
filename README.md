41a33f0536e9530efd43a0311b6af9ac9bdb6611
Stephen Benner <sbenner9@gmail.com>
2015-02-10 09:22:30 -0800

    updated README, changed LICENSE to GPLv3, performed various other refinements

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
 # iTunr
-This is a small utility script I wrote to solve a mindless problem - my iTunes library had accumulated several hundred missing file links. Astonishingly, there was no free, easy solution available on the internets to solve this problem. I looked through the available ruby gems, and there were a few duplicate removers, but nothing that could help me with this issue.
+This is a small utility script I wrote to solve a simple yet ***ridiculous*** iTunes issue. The problem was that I had accumulated several hundred missing file links--songs that appear as normal in the UI, but fail to play due to a missing file. Astonishingly, my internet search failed to turn up a single free & effective solution. I looked through the available ruby gems, and there were a few duplicate removers, but nothing that could help me with this issue in particular. Enter iTunr!
 
 ### Usage
-```bash
-bundle install && ruby itunr.rb
-```
+Right now the script only does one thing; just run it.
+
+    bundle install && ruby itunr.rb
+
 
 ## Features
-- Cleans out dead links to songs that are missing from your hard drive
-- Creates backups of all files it modifies, just in case
+- Removes all songs from your iTunes Library that are dead links (missing original file)
+- Creates backups of any files modified on your system, just in case
 
 ## TODO:
-- Incorporate features
+- New features
     - [awesome OS X patch] to keep iTunes from monopolizing control over the pause/play media key (*remember to give due credit + linkback ofc*)
 - Add tests
-- Make this into a gem
-- Add rake tasks
-	- install as binary executable
-	- output status of duplicates
-- ADD CLI capability
-- YAML config
-- export log in JSON (and output YAML by default)
+- Make this into a gem?
+- Add CLI capability
+- Add YAML config
+- Logging
 
 
 [awesome OS X patch]:http://www.thebitguru.com/projects/iTunesPatch
--- itunr.rb
+++ itunr.rb
@@ -1,22 +1,39 @@
 #!/usr/bin/env ruby
+#
+# iTunr - a small utility for iTunes
+# Copyright (C) 2015 Stephen C. Benner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 require 'bundler'
 require 'uri'
 require 'fileutils'
 Bundler.require
 
-ITUNES_DIR = "#{ENV['HOME']}/Music/iTunes"
-LIBRARY_FILE = File.join ITUNES_DIR, 'iTunes Music Library.xml'
+ITUNES_DIR        = "#{ENV['HOME']}/Music/iTunes"
+LIBRARY_FILE      = File.join ITUNES_DIR, 'iTunes Music Library.xml'
 LIBRARY_DATA_FILE = File.join ITUNES_DIR, 'iTunes Library.itl'
 
 lib = Plist::parse_xml LIBRARY_FILE
 
 valid_files = lib['Tracks'].select do |track, data|
 	filepath = data['Location']
-	# decode file path, which is encoded as a URI, not very file-system friendly
+	# decode the file path (which is encoded as a URI)
 	file = URI.decode filepath
 	# normalize file path by pruning URI info
 	file = file.sub(/file:\/\/localhost/, '')
-	# check to see if file has been removed or not, the condition for 'select'
+	# check to see if file has been removed or not (condition for the 'select' loop)
 	File.exist? file
 end
 
@@ -39,5 +56,5 @@ end
 
 IO.write LIBRARY_FILE, lib.to_plist # replace library plist file with purged plist
 
-puts "Cleaned #{missing_track_count} missing file records from iTunes Library."
-nil # suppress output
+puts "Cleaned #{missing_track_count} missing file records from your iTunes Library."
+nil # suppress expression output
